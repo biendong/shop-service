a45863fb6a40088c3a0715bd986ebc1b06d10f15
Hai Tran Dong <hai.trandong@efundamentals.com>
2023-10-08 21:25:27 +0700

    fix ut

--- src/test/java/com/digital/shop/ShopServiceApplicationTests.java
+++ src/test/java/com/digital/shop/ShopServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.digital.shop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.shop.ShopServiceApplication;
+
+@SpringBootTest(classes = ShopServiceApplication.class)
 class ShopServiceApplicationTests {
 
 	@Test
